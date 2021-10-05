fbc114a81119fdfcb1ceee5cb56da89e0d562f53
“kulkarohan” <53792428+kulkarohan@users.noreply.github.com>
2021-10-05 19:08:46 -0400

    [refactor] add 'Nonexistent' status to optimize proposal existence check

--- contracts/ZoraProposalManager.sol
+++ contracts/ZoraProposalManager.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.5;
 
 contract ZoraProposalManager {
     enum ProposalStatus {
+        Nonexistent,
         Pending,
         Passed,
         Failed,
@@ -55,7 +56,7 @@ contract ZoraProposalManager {
     function registerModule(address _proposalAddress) public onlyRegistrar {
         Proposal storage proposal = proposedModuleToProposal[_proposalAddress];
 
-        require(proposal.proposer != address(0), "ZPM::registerModule proposal does not exist");
+        require(proposal.status != ProposalStatus.Nonexistent, "ZPM::registerModule proposal does not exist");
         require(proposal.status == ProposalStatus.Pending, "ZPM::registerModule can only register pending proposals");
 
         proposal.status = ProposalStatus.Passed;
@@ -66,7 +67,7 @@ contract ZoraProposalManager {
     function cancelProposal(address _proposalAddress) public onlyRegistrar {
         Proposal storage proposal = proposedModuleToProposal[_proposalAddress];
 
-        require(proposal.proposer != address(0), "ZPM::cancelProposal proposal does not exist");
+        require(proposal.status != ProposalStatus.Nonexistent, "ZPM::cancelProposal proposal does not exist");
         require(proposal.status == ProposalStatus.Pending, "ZPM::cancelProposal can only cancel pending proposals");
 
         proposal.status = ProposalStatus.Failed;
@@ -77,7 +78,7 @@ contract ZoraProposalManager {
     function freezeProposal(address _proposalAddress) public onlyRegistrar {
         Proposal storage proposal = proposedModuleToProposal[_proposalAddress];
 
-        require(proposal.proposer != address(0), "ZPM::freezeProposal proposal does not exist");
+        require(proposal.status != ProposalStatus.Nonexistent, "ZPM::freezeProposal proposal does not exist");
         require(proposal.status == ProposalStatus.Passed, "ZPM::freezeProposal can only freeze passed proposals");
 
         proposal.status = ProposalStatus.Frozen;
