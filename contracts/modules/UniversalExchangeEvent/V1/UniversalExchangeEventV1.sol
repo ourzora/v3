009f52117310d019a73885eebd5a920f44705bae
“kulkarohan” <53792428+kulkarohan@users.noreply.github.com>
2021-10-18 18:10:28 -0400

    [feature] UEE NatSpec documentation

--- contracts/modules/UniversalExchangeEvent/V1/UniversalExchangeEventV1.sol
+++ contracts/modules/UniversalExchangeEvent/V1/UniversalExchangeEventV1.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.5;
 
+/// @title UniversalExchangeEvent V1
+/// @author kulkarohan <rohan@zora.co>
+/// @notice This module generalizes indexing of all token exchanges across the protocol
 contract UniversalExchangeEventV1 {
+    /// @notice A ExchangeDetails object that tracks a token exchange
+    /// @member tokenContract The address of the token contract
+    /// @member tokenID The ID of the token
+    /// @member amount The amount of tokens being exchanged
     struct ExchangeDetails {
-        address tokenContract; // Address of the token contract
-        uint256 tokenID; // (Optional) tokenID being exchanged
-        uint256 amount; // (Optional) The amount of tokens being exchanged
+        address tokenContract;
+        uint256 tokenID;
+        uint256 amount;
     }
 
     event ExchangeExecuted(address indexed userA, address indexed userB, ExchangeDetails a, ExchangeDetails b);
