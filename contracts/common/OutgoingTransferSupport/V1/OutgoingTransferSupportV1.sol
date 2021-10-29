d82cc50375350b4e7449ba90ff7d4422d1d4251f
tysonbattistella <tysonbattistella@gmail.com>
2021-10-28 22:52:07 -0400

    [feature] RoyaltyPayoutSupportV1 implementation and OutgoingTransferSupportV1 implementation

---
+++ contracts/common/OutgoingTransferSupport/V1/OutgoingTransferSupportV1.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.5;
+
+import {IWETH} from "../../../interfaces/common/IWETH.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/// @title OutgoingTransferSupportV1
+/// @author tbtstl <t@zora.co>
+/// @notice This contract extension supports paying out funds to an external recipient
+contract OutgoingTransferSupportV1 {
+    using SafeERC20 for IERC20;
+
+    IWETH weth;
+
+    constructor(address _wethAddress) {
+        weth = IWETH(_wethAddress);
+    }
+
+    /// @notice Handle an outgoing funds transfer
+    /// @dev Wraps ETH in WETH if the receiver cannot receive ETH, noop if the funds to be sent are 0 or recipient is invalid
+    /// @param _dest The destination for the funds
+    /// @param _amount The amount to be sent
+    /// @param _currency The currency to send funds in, or address(0) for ETH
+    /// @param _gasLimit The gas limit to use when attempting a payment (if 0, gasleft() is used)
+    function _handleOutgoingTransfer(
+        address _dest,
+        uint256 _amount,
+        address _currency,
+        uint256 _gasLimit
+    ) internal {
+        if (_amount == 0 || _dest == address(0)) {
+            return;
+        }
+
+        // If no gas limit was provided or provided gas limit greater than gas left, just use the remaining gas.
+
+        // Handle ETH payment
+        if (_currency == address(0)) {
+            require(address(this).balance >= _amount, "_handleOutgoingTransfer insolvent");
+
+            uint256 gas = (_gasLimit == 0 || _gasLimit > gasleft()) ? gasleft() : _gasLimit;
+            (bool success, ) = _dest.call{value: _amount, gas: gas}(new bytes(0));
+            // If the ETH transfer fails (sigh), wrap the ETH and try send it as WETH.
+            if (!success) {
+                weth.deposit{value: _amount}();
+                IERC20(address(weth)).safeTransfer(_dest, _amount);
+            }
+        } else {
+            IERC20(_currency).safeTransfer(_dest, _amount);
+        }
+    }
+}
--- contracts/common/RoyaltyPayoutSupport/V1/RoyaltyPayoutSupportV1.sol
+++ contracts/common/RoyaltyPayoutSupport/V1/RoyaltyPayoutSupportV1.sol
@@ -1,39 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.5;
 
-import {IZoraV1Market, IZoraV1Media} from "../../../interfaces/common/IZoraV1.sol";
 import {IRoyaltyEngineV1} from "../../../external/royalty-registry/contracts/IRoyaltyEngineV1.sol";
-import {IWETH} from "../../../interfaces/common/IWETH.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {OutgoingTransferSupportV1} from "../../OutgoingTransferSupport/V1/OutgoingTransferSupportV1.sol";
 
 /// @title RoyaltySupportV1
 /// @author tbtstl <t@zora.co>
-/// @notice This contract extension supports paying out royalties using the Manifold Royalty Registry and Zora V1 protocol
-contract RoyaltyPayoutSupportV1 {
-    IZoraV1Media zoraV1Media;
-    IZoraV1Market zoraV1Market;
+/// @notice This contract extension supports paying out royalties using the Manifold Royalty Registry
+contract RoyaltyPayoutSupportV1 is OutgoingTransferSupportV1 {
     IRoyaltyEngineV1 royaltyEngine;
-    IWETH weth;
 
-    /// @param _zoraV1ProtocolMedia The ZORA NFT Protocol Media Contract address
     /// @param _royaltyEngine The Manifold Royalty Engine V1 address
     /// @param _wethAddress WETH token address
-    constructor(
-        address _zoraV1ProtocolMedia,
-        address _royaltyEngine,
-        address _wethAddress
-    ) {
-        zoraV1Media = IZoraV1Media(_zoraV1ProtocolMedia);
-        zoraV1Market = IZoraV1Market(zoraV1Media.marketContract());
+    constructor(address _royaltyEngine, address _wethAddress) OutgoingTransferSupportV1(_wethAddress) {
         royaltyEngine = IRoyaltyEngineV1(_royaltyEngine);
-        weth = IWETH(_wethAddress);
     }
 
     /// @notice Pays out royalties for given NFTs
-    /// @param _tokenContract The NFT Contract to get royalty information from
+    /// @param _tokenContract The NFT contract address to get royalty information from
     /// @param _tokenId, The Token ID to get royalty information from
     /// @param _amount The total sale amount
     /// @param _payoutCurrency The ERC-20 token address to payout royalties in, or address(0) for ETH
     /// @param _gasLimit The gas limit to use when attempting to payout royalties. Uses gasleft() if not provided.
+    /// @return remaining funds after paying out royalties
     function _handleRoyaltyPayout(
         address _tokenContract,
         uint256 _tokenId,
@@ -41,50 +31,45 @@ contract RoyaltyPayoutSupportV1 {
         address _payoutCurrency,
         uint256 _gasLimit
     ) internal returns (uint256) {
-        if (_tokenContract == address(zoraV1Media)) {
-            return _handleZoraPayout(_tokenId, _amount, _payoutCurrency);
-        } else {
-            // If no gas limit was provided or provided gas limit greater than gas left, just pass the remaining gas.
-            uint256 gas = (_gasLimit == 0 || _gasLimit > gasleft()) ? gasleft() : _gasLimit;
+        // If no gas limit was provided or provided gas limit greater than gas left, just pass the remaining gas.
+        uint256 gas = (_gasLimit == 0 || _gasLimit > gasleft()) ? gasleft() : _gasLimit;
+        uint256 remainingFunds = _amount;
 
-            try this._handleRoyaltyEnginePayout{gas: gas, value: msg.value}(_tokenContract, _tokenId, _amount, _payoutCurrency) returns (
-                uint256 _remainingFunds
-            ) {
-                return _remainingFunds;
-            } catch {
-                return _amount;
-            }
+        try this._handleRoyaltyEnginePayout{gas: gas, value: msg.value}(_tokenContract, _tokenId, _amount, _payoutCurrency) returns (uint256 _remainingFunds) {
+            remainingFunds = _remainingFunds;
+        } catch {
+            remainingFunds = _amount;
         }
 
-        return 1;
-    }
-
-    /// @notice Pays out royalties for ZORA V1 NFTs by executing a sale on the underlying market
-    /// @param _tokenId, The Token ID to get royalty information from
-    /// @param _amount The total sale amount
-    /// @param _payoutCurrency The ERC-20 token address to payout royalties in, or address(0) for ETH
-    function _handleZoraPayout(
-        uint256 _tokenId,
-        uint256 _amount,
-        address _payoutCurrency
-    ) private returns (uint256) {
-        return 1;
+        return remainingFunds;
     }
 
     /// @notice Pays out royalties for NFTs based on the information returned by the royalty engine
-    /// @dev This method is public to enable setting a gas limit when called - see `_handleRoyaltyPayout`.
+    /// @dev This method is external to enable setting a gas limit when called - see `_handleRoyaltyPayout`.
     /// @param _tokenContract The NFT Contract to get royalty information from
     /// @param _tokenId, The Token ID to get royalty information from
     /// @param _amount The total sale amount
     /// @param _payoutCurrency The ERC-20 token address to payout royalties in, or address(0) for ETH
+    /// @return remaining funds after paying out royalties
     function _handleRoyaltyEnginePayout(
         address _tokenContract,
         uint256 _tokenId,
         uint256 _amount,
         address _payoutCurrency
-    ) public payable returns (uint256) {
+    ) external payable returns (uint256) {
         require(msg.sender == address(this), "_handleRoyaltyEnginePayout only self callable");
+        uint256 remainingAmount = _amount;
 
-        return 1;
+        (address payable[] memory recipients, uint256[] memory amounts) = royaltyEngine.getRoyalty(_tokenContract, _tokenId, _amount);
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            // Ensure that we aren't somehow paying out more than we have
+            require(remainingAmount >= amounts[i], "insolvent");
+            _handleOutgoingTransfer(recipients[i], amounts[i], _payoutCurrency, 0);
+
+            remainingAmount -= amounts[i];
+        }
+
+        return remainingAmount;
     }
 }
