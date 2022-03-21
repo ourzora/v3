df6db95a43399615fca8d9845d71ad130c0f20f0
Rohan Kulkarni <rohan@zora.co>
2022-03-21 19:17:44 -0400

    chore: remove unused files

--- contracts/interfaces/common/IERC2981.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.10;
-
-interface IERC2981 {
-    function royaltyInfo(uint256 _tokenId, uint256 _salePrice) external view returns (address receiver, uint256 royaltyAmount);
-}
