fda436d523eebb616d8b1bd97b6fa6240d6ccd24
James Geary <36774175+jgeary@users.noreply.github.com>
2022-06-07 13:23:21 -0400

    interface

--- contracts/modules/Asks/Omnibus/IAsksOmnibus.sol
+++ contracts/modules/Asks/Omnibus/IAsksOmnibus.sol
@@ -7,5 +7,26 @@ import {AsksDataStorage} from "./AsksDataStorage.sol";
 /// @author kulkarohan
 /// @notice Interface for Reserve Auction Core ERC-20
 interface IAsksOmnibus {
+    function createAsk(
+        address _tokenContract,
+        uint256 _tokenId,
+        uint256 _expiry,
+        uint256 _askPrice,
+        address _sellerFundsRecipient,
+        address _askCurrency,
+        address _buyer,
+        uint16 _findersFeeBps,
+        AsksDataStorage.ListingFee memory _listingFee,
+        AsksDataStorage.TokenGate memory _tokenGate
+    ) external;
 
+    function cancelAsk(address _tokenContract, uint256 _tokenId) external;
+
+    function fillAsk(
+        address _tokenContract,
+        uint256 _tokenId,
+        uint256 _price,
+        address _currency,
+        address _finder
+    ) external payable;
 }
